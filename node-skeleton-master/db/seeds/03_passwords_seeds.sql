367a164fd91cd9e62d88e8d59454d24fce8ab3bd
Val Hustle <stars@MacBook-Air.local>
2022-01-10 14:43:03 -0500

    added created at and updated at fields as well as entry data

--- node-skeleton-master/db/seeds/03_passwords_seeds.sql
+++ node-skeleton-master/db/seeds/03_passwords_seeds.sql
@@ -1,8 +1,8 @@
 -- Passwords_storage table seeds here (Example)
-INSERT INTO passwords_storage (id, creator_id, username, password, website_name, website_category, organization_id) VALUES
+INSERT INTO passwords_storage (id, creator_id, username, password, website_name, website_category, organization_id, created_at, updated_at) VALUES
 
 -- Recovery email referenced from users.sql
-(1, 1, 'Facebook_username', 'SecretPass','www.facebook.com', 'social', 1 ),
-(2, 2, 'Instagram_username', 'SuperPa$$','www.instagram.com', 'social', 2),
-(3, 3, 'TD_bank_user','$uperB', 'www.td-bank.com', 'banking', 2),
-(4, 3, 'Netflix_user','Net$uperB', 'www.netflix.com', 'entertainment', 2);
+(1, 1, 'Facebook_username', 'SecretPass','www.facebook.com', 'social', 1, now(), now()),
+(2, 2, 'Instagram_username', 'SuperPa$$','www.instagram.com', 'social', 2, now(), now()),
+(3, 3, 'TD_bank_user','$uperB', 'www.td-bank.com', 'banking', 2, now(), now()),
+(4, 3, 'Netflix_user','Net$uperB', 'www.netflix.com', 'entertainment', 2, now(), now());
