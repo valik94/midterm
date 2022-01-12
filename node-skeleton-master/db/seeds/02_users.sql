a97c5d9daecb7d75d12279bb90780b7f2b26ef66
Val Hustle <stars@MacBook-Air.local>
2022-01-11 23:13:36 -0500

    changing DB structure based on discussion with team on frontend elements needs and storage requirements to simplify query and ajax requests

--- node-skeleton-master/db/seeds/02_users.sql
+++ node-skeleton-master/db/seeds/02_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, username, email, login_password, organization_id, created_at) VALUES
-('Alice', 'AliceInWonderland', 'alice@wonderland.com', 'SecretPass', 1, now()),
-('Kira', 'KiraPasswordKeepa','Kira@gmail.com', 'SuperPa$$', 2, now()),
-('Bob', 'BobTheKeepar','BobK@gmail.com', '$uperB', 2, now());
+INSERT INTO users (email, master_password, organisation_id, created_at) VALUES
+('alice@wonderland.com', 'SecretPass', 1, now()),
+('Kira@gmail.com', 'SuperPa$$', 2, now()),
+('BobK@gmail.com', '$uperB', 2, now());
--- node-skeleton-master/db/seeds/03_passwords_seeds.sql
+++ node-skeleton-master/db/seeds/03_passwords_seeds.sql
@@ -1,8 +1,8 @@
 -- Passwords_storage table seeds here (Example)
-INSERT INTO passwords_storage (id, creator_id, username, password, website_name, website_category, organization_id, created_at, updated_at) VALUES
+INSERT INTO passwords_storage (id, user_id, password_text, url, category, organisation_id, created_at, updated_at) VALUES
 
 -- demo data for the passwords storage database
-(1, 1, 'Facebook_username', 'SecretPass','www.facebook.com', 'social', 1, now(), now()),
-(2, 2, 'Instagram_username', 'SuperPa$$','www.instagram.com', 'social', 2, now(), now()),
-(3, 3, 'TD_bank_user','$uperB', 'www.td-bank.com', 'banking', 2, now(), now()),
-(4, 3, 'Netflix_user','Net$uperB', 'www.netflix.com', 'entertainment', 2, now(), now());
+(1, 1, 'SecretPass','www.facebook.com', 'social', 1, now(), now()),
+(2, 2, 'SuperPa$$','www.instagram.com', 'social', 2, now(), now()),
+(3, 3, '$uperB', 'www.td-bank.com', 'banking', 2, now(), now()),
+(4, 3,'Net$uperB', 'www.netflix.com', 'entertainment', 2, now(), now());
