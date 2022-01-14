82a5ef1e105b8181d682ea3eb3962a057b35f1eb
Val Hustle <stars@MacBook-Air.local>
2022-01-13 21:28:07 -0500

    fixing routes to /login to ensure userId is passed correctly to the db query and the page redirects to dashboards. After many iterations was successful.

--- node-skeleton-master/routes/index.js
+++ node-skeleton-master/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const { isAuthenticated, getPasswordsbyUsers, sortUserPasswords } = require("../helpers.js");
+const { isAuthenticated, getPasswordsByUsers, sortUserPasswords } = require("../helpers.js");
 
 
 module.exports = (db) => {
