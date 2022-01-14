eb3c9163aa25754eca72c0a402f24d3bc3c76bef
Val Hustle <stars@MacBook-Air.local>
2022-01-13 21:02:34 -0500

    added 4th table to SQL to simplify relationship between users and organisations

---
+++ node-skeleton-master/db/seeds/04_users_organisations.sql
@@ -0,0 +1,4 @@
+INSERT INTO users_organisations (user_id, organisations_id) VALUES (1, 1);
+INSERT INTO users_organisations (user_id, organisations_id) VALUES (1, 2);
+INSERT INTO users_organisations (user_id, organisations_id) VALUES (2, 3);
+INSERT INTO users_organisations (user_id, organisations_id) VALUES (3, 3);
