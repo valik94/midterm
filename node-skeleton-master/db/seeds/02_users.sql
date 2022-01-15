37cba2ddc181eb334fe52f8a78110c4e504c66e5
S550Stang <prabhuvardhanrao94@gmail.com>
2022-01-15 02:31:21 +0000

    fix errors

--- node-skeleton-master/db/seeds/02_users.sql
+++ node-skeleton-master/db/seeds/02_users.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (email, master_password) VALUES ('prabhu@gmail.com', '123');
 INSERT INTO users (email, master_password) VALUES ('valentin@gmail.com', '321');
 INSERT INTO users (email, master_password) VALUES ('michael@gmail.com', '123');
-INSERT INTO users (email, master_password) VALUES ('lhl@gmail.com, '321');
+INSERT INTO users (email, master_password) VALUES ('lhl@gmail.com', '321');
