e7de72024fc8e94d58ad1604e96911ff1bd1f1c8
S550Stang <prabhuvardhanrao94@gmail.com>
2022-01-15 01:52:37 +0000

    modularize code and add comments accordingly

--- node-skeleton-master/routes/index.js
+++ node-skeleton-master/routes/index.js
@@ -1,34 +1,38 @@
 const express = require('express');
-const router  = express.Router();
+const indexRoute = express.Router();
 const app = express();
-app.set("view engine", "ejs"); //FOR FRONTEND HEADER ON EJS
-const { isAuthenticated, getPasswordsByUsers, sortUserPasswords } = require("../helpers.js");
+app.set("view engine", "ejs");
+const { isAuthenticated, getPasswordsbyUsers, sortUserPasswords } = require("../helpers.js");
 
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1'],
 
-module.exports = (db) => {
+  maxAge: 24 * 60 * 60 * 1000
+}));
 
 /* GET route - query database for passwords related to user and organisation
    pass through templateVars into EJS file to render information for specific user/organisation */
-   router.get("/", (req, res) => {
-    const id = req.session.user_id;
-    const idIsExisting = isAuthenticated(id, db);
-    idIsExisting.then((value) => {
+indexRoute.get("/", (req, res) => {
+  const id = req.session.user_id;
+  const idIsExisting = isAuthenticated(id);
+  idIsExisting.then((value) => {
 
-      if (!value) {
-        res.redirect('/login');
-      } else {
-        getPasswordsByUsers(value, db)
-        .then((passwordsByUser) => {
-          const sortedpasswordsByUser = sortUserPasswords(passwordsByUser, db);
-          console.log('ALL THE PASSWORDS HERE: ', sortedpasswordsByUser);
-          const templateVars = { value: id, users: sortedpasswordsByUser };
-          res.render("dashboard", templateVars); //THIS SENDS TO DASHBOARD.EJS INSTEAD OF INDEX.EJS
-        }).catch(error => {
-          console.log(error)
-        });
-      }
-    })
-  });
-  return router;
-  };
+    if (!value) {
+      res.redirect('/login');
+    } else {
+      getPasswordsbyUsers(value)
+      .then((passwordsByUser) => {
+        const sortedpasswordsByUser = sortUserPasswords(passwordsByUser);
+        console.log('ALL THE PSWORDS HERE: ', sortedpasswordsByUser);
+        const templateVars = { value: id, users: sortedpasswordsByUser };
+        res.render("index", templateVars);
+      }).catch(error => {
+        console.log(error)
+      });
+    }
+  })
+});
 
+module.exports = indexRoute;
--- node-skeleton-master/routes/login.js
+++ node-skeleton-master/routes/login.js
@@ -1,81 +1,67 @@
 const express = require('express');
-const res = require('express/lib/response');
-const router  = express.Router();
-const { isAuthenticated, emailExists, passwordValidator } = require("../helpers.js");
 const app = express();
+const loginRoute = express.Router();
+const { emailExists, passwordValidator } = require("../helpers.js");
 
 /* require and use cookie session to store user ids for cookie sessions
  * https://www.npmjs.com/package/cookie-session */
- const cookieSession = require('cookie-session');
- app.use(cookieSession({
-   name: 'session',
-   keys: ['key1'],
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1'],
 
-   maxAge: 24 * 60 * 60 * 1000
- }));
+  maxAge: 24 * 60 * 60 * 1000
+}));
 
+/* GET route
+ * https://stackoverflow.com/questions/29941208/express-error-typeerror-router-use-requires-middleware-function-but-got-a-o */
+loginRoute.get("/", (req, res) => {
+  const templateVars = { value: false };
+  res.render("login", templateVars);
+});
 
-module.exports = (db) => {
-
-  // router.post("/", (req, res) => {
-  //   res.render("dashboard");
-  // });
-  router.get('/', (req, res) => {
-    const id = req.session.user_id;
-    const idIsExisting = isAuthenticated(id, db);
-    idIsExisting.then((value) => {
-
-      if (value) {
-        res.redirect('/index');
-      }
-      const templateVars = {value: false};
-
-    res.render('homepage', templateVars)
-  })
-})
-
-  router.post("/", (req, res) => {
-    const { email, password } = req.body;
-    const errors = {
-      email: "Must provide email!",
-      password: "Must provide password!",
-      emailOrPwinvalid: "Email or Password is invalid!",
-    }; //This is for the popup errors
-    console.log('TEST: router.post', req.body);
-
-    if (!email) {
-      res.send(errors.email);
-      return;
-    } else if (!password) {
-      res.send(errors.password);
-      return;
-    }
-
-    /* promise chain that will first check if the users email exists against our db and then validate their password
-     * async error handling for when a username or password is invalid will happen in here - TODO */
-    let validUserEmail = emailExists(email, db);
-    validUserEmail.then((value) => {
-
-      if (!value) {
-        res.send(errors.emailOrPwinvalid);
-        throw new Error('email does not exist');
-      } else {
-        return passwordValidator(password, email, db);
-      }
-    }).then((value) => {
-
-      if (!value) {
-        res.send(errors.emailOrPwinvalid);
-        throw new Error('password does not exist');
-      } else {
-        req.session.user_id = value;
-        // res.render('dashboard')
-        res.json({status: "Success", redirect: '/'});
-      }
-    }).catch(error => {
-      console.log(error);
-    });
-  });
-
-  return router;
+/* POST route
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+   syncronous error handling before our promise chain below even starts */
+loginRoute.post("/", (req, res) => {
+  const { email, password } = req.body;
+  const errors = {
+    email: "Must provide email!",
+    password: "Must provide password!",
+    emailOrPwinvalid: "Email or Password is invalid!",
   };
+
+  if (!email) {
+    res.send(errors.email);
+    return;
+  } else if (!password) {
+    res.send(errors.password);
+    return;
+  }
+
+  /* promise chain that will first check if the users email exists against our db and then validate their password
+   * async error handling for when a username or password is invalid will happen in here - TODO */
+  let validUserEmail = emailExists(email);
+  validUserEmail.then((value) => {
+
+    if (!value) {
+      res.send(errors.emailOrPwinvalid);
+      throw new Error('email does not exist');
+    } else {
+      return passwordValidator(password, email);
+    }
+  }).then((value) => {
+
+    if (!value) {
+      res.send(errors.emailOrPwinvalid);
+      throw new Error('password does not exist');
+    } else {
+      req.session.user_id = value;
+      res.json({status: "Success", redirect: '/'});
+    }
+  }).catch(error => {
+    console.log(error);
+  });
+});
+
+module.exports = loginRoute;
