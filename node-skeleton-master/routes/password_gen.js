1c147881a90059ed061101a76c1acb0dd04db60f
S550Stang <prabhuvardhanrao94@gmail.com>
2022-01-16 18:01:49 +0000

    local change

--- node-skeleton-master/routes/password_gen.js
+++ node-skeleton-master/routes/password_gen.js
@@ -6,21 +6,28 @@
  */
 
 // include db module from dbConn.js
-const express = require('express');
+const express = require("express");
 const passwordRouter = express.Router();
-const generator = require('generate-password');
+const generator = require("generate-password");
 const app = express();
-const { db, Pool } = require('../db/dbConn');
-const { isAuthenticated, getUserOrganizations, newPasswordToDatabase, getOrgIdFromName } = require("../helpers.js");
+// const { db, Pool } = require('../db/dbConn'); /new change /
+const {
+  isAuthenticated,
+  getUserOrganizations,
+  newPasswordToDatabase,
+  getOrgIdFromName,
+} = require("../helpers.js");
 
 // require and use cookie session to store user ids for cookie sessions
-const cookieSession = require('cookie-session');
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1'],
+const cookieSession = require("cookie-session");
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1"],
 
-  maxAge: 24 * 60 * 60 * 1000
-}));
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
 
 /* get routes for password generator page
  * query to db for the current logged in user, and check what orginizations they are part of
@@ -29,19 +36,20 @@ passwordRouter.get("/", (req, res) => {
   const id = req.session.user_id;
 
   isAuthenticated(id)
-  .then((userId) => {
-    if (!userId) {
-      res.redirect('/login');
-    }
-    return getUserOrganizations(userId);
-  })
-  .then((usersOrgs) => {
-    const organisations = [...usersOrgs];
-    const templateVars = { value: id, organisations };
-    res.render("password_gen", templateVars);
-  }).catch(error => {
-    console.log(error);
-  });
+    .then((userId) => {
+      if (!userId) {
+        res.redirect("/login");
+      }
+      return getUserOrganizations(userId);
+    })
+    .then((usersOrgs) => {
+      const organisations = [...usersOrgs];
+      const templateVars = { value: id, organisations };
+      res.render("password_gen", templateVars);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 // POSTS routes - TODO - take in db here - TEST
@@ -49,55 +57,64 @@ passwordRouter.post("/", (req, res) => {
   const id = req.session.user_id;
 
   if (req.body.length) {
-  const passwordGenerator = function () {
+    const passwordGenerator = function () {
+      if (req.body.uppercase === "true") {
+        uppercaseBoolean = true;
+      } else if (req.body.uppercase === "false") {
+        uppercaseBoolean = false;
+      }
 
-    if (req.body.uppercase === 'true') {
-      uppercaseBoolean = true;
-    } else if (req.body.uppercase === 'false') {
-      uppercaseBoolean = false;
-    }
+      if (req.body.lowercase === "true") {
+        lowercaseBoolean = true;
+      } else if (req.body.lowercase === "false") {
+        lowercaseBoolean = false;
+      }
 
-    if (req.body.lowercase === 'true') {
-      lowercaseBoolean = true;
-    } else if (req.body.lowercase === 'false') {
-      lowercaseBoolean = false;
-    }
+      if (req.body.symbols === "true") {
+        symbolsBoolean = true;
+      } else if (req.body.symbols === "false") {
+        symbolsBoolean = false;
+      }
 
-    if (req.body.symbols === 'true') {
-      symbolsBoolean = true;
-    } else if (req.body.symbols=== 'false') {
-      symbolsBoolean = false;
-    }
+      if (req.body.numbers === "true") {
+        numbersBoolean = true;
+      } else if (req.body.numbers === "false") {
+        numbersBoolean = false;
+      }
 
-    if (req.body.numbers === 'true') {
-      numbersBoolean = true;
-    } else if (req.body.numbers === 'false') {
-      numbersBoolean = false;
-    }
+      return (password = generator.generate({
+        length: req.body.length,
+        numbers: numbersBoolean,
+        uppercase: uppercaseBoolean,
+        lowercase: lowercaseBoolean,
+        symbols: symbolsBoolean,
+      }));
+    };
 
-    return password = generator.generate({
-      length: req.body.length,
-      numbers: numbersBoolean,
-      uppercase: uppercaseBoolean,
-      lowercase: lowercaseBoolean,
-      symbols: symbolsBoolean
-    });
-  };
-
-  const thePassword = passwordGenerator();
-  console.log("thepassword? ", thePassword);
-  getOrgIdFromName(req.body.organisationName)
-    .then((val) => {
+    const thePassword = passwordGenerator();
+    console.log("thepassword? ", thePassword);
+    getOrgIdFromName(req.body.organisationName).then((val) => {
       const orgId = val;
-      newPasswordToDatabase(id, orgId, req.body.category, req.body.url, thePassword);
-      res.send('This worked!');
+      newPasswordToDatabase(
+        id,
+        orgId,
+        req.body.category,
+        req.body.url,
+        thePassword
+      );
+      res.send("This worked!");
     });
   } else {
-    getOrgIdFromName(req.body.organisationName)
-    .then((val) => {
+    getOrgIdFromName(req.body.organisationName).then((val) => {
       const orgId = val;
-      newPasswordToDatabase(id, orgId, req.body.category, req.body.url, req.body.password);
-      res.send('This also worked! You can submit your own password');
+      newPasswordToDatabase(
+        id,
+        orgId,
+        req.body.category,
+        req.body.url,
+        req.body.password
+      );
+      res.send("This also worked! You can submit your own password");
     });
   }
 });
